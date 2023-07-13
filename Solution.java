41b0ef83177496aab219a7aa146bd141fcbb5533
UkarainskiyS <https://github.com/UkrainskiyS>
2023-07-13 18:40:39 +0300

    refactor decoder

--- Solution.java
+++ Solution.java
@@ -1,15 +1,13 @@
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 public class Solution {
 
 	private static final Base64Decoder base64Decoder = new Base64Decoder();
 
 	public static void main(String[] args) {
-		System.out.println(base64Decoder.decodePacket("IgP_fwcDAQhTV0lUQ0gwMQMFREVWMDEFREVWMDIFREVWMDO1"));
+		System.out.println(base64Decoder.decodePacket("OAL_fwQCAghTRU5TT1IwMQ8EDGQGT1RIRVIxD7AJBk9USEVSMgCsjQYGT1RIRVIzCAAGT1RIRVI09w"));
 	}
 }
 
@@ -18,36 +16,34 @@ final class Base64Decoder {
 
 	public Packet decodePacket(String base64) {
 		final byte[] bytes = decoder.decode(base64.getBytes(StandardCharsets.UTF_8));
-		final int length = TypeConverter.readByte(bytes[0]);
-		final Payload payload = decodePayload(Arrays.copyOfRange(bytes, 1, length + 1));
-		final int crc8 = TypeConverter.readByte(bytes[length + 1]);
+		final TByte length = new TByte(bytes[0]);
+		final Payload payload = decodePayload(Arrays.copyOfRange(bytes, 1, length.val() + 1));
+		final TByte crc8 = new TByte(bytes[length.val() + 1]);
 		return new Packet(length, payload, crc8);
 	}
 
 	private Payload decodePayload(byte[] bytes) {
-		System.out.println(Arrays.toString(bytes));
-
 		int readIndex = 0;
 
-		final Pair<Long, Integer> src = TypeConverter.readUleb128(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-		readIndex += src.two();
+		final Varuint src = new Varuint(Arrays.copyOfRange(bytes, readIndex, bytes.length));
+		readIndex += src.countBytes();
 
-		final Pair<Long, Integer> dst = TypeConverter.readUleb128(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-		readIndex += dst.two();
+		final Varuint dst = new Varuint(Arrays.copyOfRange(bytes, readIndex, bytes.length));
+		readIndex += dst.countBytes();
 
-		final Pair<Long, Integer> serial = TypeConverter.readUleb128(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-		readIndex += serial.two();
+		final Varuint serial = new Varuint(Arrays.copyOfRange(bytes, readIndex, bytes.length));
+		readIndex += serial.countBytes();
 
-		final DevType dev_type = DevType.of(TypeConverter.readByte(bytes[readIndex++]));
+		final DevType dev_type = DevType.of(new TByte(bytes[readIndex++]).val());
 
-		final Command cmd = Command.of(TypeConverter.readByte(bytes[readIndex++]));
+		final Cmd cmd = Cmd.of(new TByte(bytes[readIndex++]).val());
 
 		final CmdBody cmd_body = decodeCmdBody(Arrays.copyOfRange(bytes, readIndex, bytes.length), dev_type, cmd);
 
-		return new Payload(src.one(), dst.one(), serial.one(), dev_type, cmd, cmd_body);
+		return new Payload(src, dst, serial, dev_type, cmd, cmd_body);
 	}
 
-	private CmdBody decodeCmdBody(byte[] bytes, DevType dev_type, Command cmd) {
+	private CmdBody decodeCmdBody(byte[] bytes, DevType dev_type, Cmd cmd) {
 		switch (cmd) {
 			case WHOISHERE:
 			case IAMHERE: {
@@ -56,217 +52,294 @@ final class Base64Decoder {
 				} else if (dev_type == DevType.Switch) {
 					return decodeCmdBodyWhereForSwitch(bytes);
 				} else {
-					return new CmdBodyDevName(TypeConverter.readString(bytes).one());
+					return new CBOnlyDevName(new TString(bytes));
 				}
 			}
 			case STATUS: {
 				if (dev_type == DevType.EnvSensor) {
-
+					//todo
 				} else if (dev_type == DevType.Switch || dev_type == DevType.Lamp || dev_type == DevType.Socket) {
-
+					//todo
 				} else {
-
+					return CmdBody.EMPTY;
 				}
 			}
-			case TICK: return new CmdBodyTick(TypeConverter.readUleb128(bytes).one());
-			default: return new CmdBodyTick(0);
+			case TICK:
+				return new CBTick(new Varuint(bytes));
+			default:
+				return CmdBody.EMPTY;
 		}
 	}
 
-	private CmdBodyWhereForSensor decodeCmdBodyWhereForSensor(byte[] bytes) {
+	private CBSensorWhere decodeCmdBodyWhereForSensor(byte[] bytes) {
 		int readIndex = 0;
 
-		final Pair<String, Integer> dev_name = TypeConverter.readString(bytes);
-		readIndex += dev_name.two();
+		final TString dev_name = new TString(bytes);
+		readIndex += dev_name.countBytes();
 
-		final int sensors = TypeConverter.readByte(bytes[readIndex++]);
+		final TByte sensors = new TByte(bytes[readIndex++]);
 
-		int count = TypeConverter.readByte(bytes[readIndex++]);
-		final List<Trigger> triggers = new ArrayList<>(count);
-		for (; count > 0; count--) {
-			final int op = TypeConverter.readByte(bytes[readIndex++]);
+		final TArray<Trigger> triggers = new TArray<>(
+			Arrays.copyOfRange(bytes, readIndex, bytes.length),
+			byteArray -> {
+				int index = 0;
 
-			final Pair<Long, Integer> value = TypeConverter.readUleb128(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-			readIndex += value.two();
+				final TByte op = new TByte(byteArray[index++]);
 
-			final Pair<String, Integer> name = TypeConverter.readString(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-			readIndex += name.two();
+				final Varuint value = new Varuint(Arrays.copyOfRange(byteArray, index, byteArray.length));
+				index += value.countBytes();
 
-			triggers.add(new Trigger(op, value.one(), name.one()));
-		}
+				final TString name = new TString(Arrays.copyOfRange(byteArray, index, byteArray.length));
+				index += name.countBytes();
 
-		return new CmdBodyWhereForSensor(dev_name.one(), new DevProps(sensors, triggers));
+				return new Pair<>(new Trigger(op, value, name), index);
+		});
+
+		return new CBSensorWhere(dev_name, new DP(sensors, triggers));
 	}
 
-	public CmdBodyWhereForSwitch decodeCmdBodyWhereForSwitch(byte[] bytes) {
+	public CBSwitchWhere decodeCmdBodyWhereForSwitch(byte[] bytes) {
 		int readIndex = 0;
 
-		final Pair<String, Integer> dev_name = TypeConverter.readString(bytes);
-		readIndex += dev_name.two();
+		final TString dev_name = new TString(bytes);
+		readIndex += dev_name.countBytes();
 
-		int count = TypeConverter.readByte(bytes[readIndex++]);
-		final List<String> names = new ArrayList<>(count);
-		for (; count > 0; count--) {
-			final Pair<String, Integer> name = TypeConverter.readString(Arrays.copyOfRange(bytes, readIndex, bytes.length));
-			readIndex += name.two();
+		final TArray<TString> names = new TArray<>(
+			Arrays.copyOfRange(bytes, readIndex, bytes.length),
+			byteArray -> {
+				int index = 0;
 
-			names.add(name.one());
-		}
+				final TString name = new TString(Arrays.copyOfRange(byteArray, index, byteArray.length));
+				index += name.countBytes();
 
-		return new CmdBodyWhereForSwitch(dev_name.one(), new DevPropsNames(names));
+				return new Pair<>(name, index);
+			});
+
+		return new CBSwitchWhere(dev_name, new DPNameList(names));
+	}
+}
+
+record Packet(TByte length, Payload payload, TByte crc8) {
+}
+
+record Payload(Varuint src, Varuint dst, Varuint serial, DevType dev_type, Cmd cmd, CmdBody cmd_body) {
+}
+
+interface CmdBody {
+	CmdBody EMPTY = new CBEmpty();
+}
+
+record CBEmpty() implements CmdBody {
+}
+
+record CBTick(Varuint timestamp) implements CmdBody {
+}
+
+record CBOnlyDevName(TString dev_name) implements CmdBody {
+}
+
+record CBSensorWhere(TString dev_name, DP dev_props) implements CmdBody {
+}
+
+record CBSwitchWhere(TString dev_name, DPNameList dev_ames) implements CmdBody {
+}
+
+record CBValue(TByte value) implements CmdBody {
+}
+
+record CBValueList(List<Varuint> values) implements CmdBody {
+}
+
+record DP(TByte sensors, TArray<Trigger> triggers) {
+}
+
+record Trigger(TByte op, Varuint value, TString name) {
+}
+
+record DPNameList(TArray<TString> dev_names) {
+}
+
+
+interface Type<T> {
+	T val();
+}
+
+interface BigType<T> extends Type<T> {
+	byte[] encode();
+	int countBytes();
+}
+
+interface LittleType<T> extends Type<T> {
+	byte encode();
+}
+
+final class TByte implements LittleType<Integer> {
+	private final Integer value;
+
+	TByte(byte b) {
+		this.value = Byte.toUnsignedInt(b);
 	}
 
+	@Override
+	public Integer val() {
+		return value;
+	}
 
-	static final class TypeConverter {
+	@Override
+	public byte encode() {
+		return value.byteValue();
+	}
 
-		public static int readByte(byte b) {
-			return Byte.toUnsignedInt(b);
+	@Override
+	public String toString() {
+		return value.toString();
+	}
+}
+
+final class TString implements BigType<String> {
+	private final String value;
+	private final int countBytes;
+
+	TString(byte[] bytes) {
+		final StringBuilder builder = new StringBuilder();
+		int count = Byte.toUnsignedInt(bytes[0]);
+		for (int i = 1; i <= count; i++) {
+			builder.appendCodePoint(bytes[i]);
 		}
 
-		/**
-		 * @param bytes 3 bytes
-		 * @return Pair: one - result, two - count reading elements
-		 */
-		public static Pair<Long, Integer> readUleb128(byte[] bytes) {
-			long value = 0;
-			int bitSize = 0;
-			int read;
+		this.value = builder.toString();
+		this.countBytes = count + 1;
+	}
 
-			int index = 0;
-			do {
-				read = bytes[index++];
-				value += ((long) read & 0x7f) << bitSize;
-				bitSize += 7;
-				if (bitSize >= 64) {
-					throw new ArithmeticException("ULEB128 value exceeds maximum value for long type.");
-				}
-			} while ((read & 0x80) != 0);
-			return new Pair<>(value, index);
-		}
+	@Override
+	public String val() {
+		return value;
+	}
 
-		/**
-		 * @return Pair: one - result, two - count reading elements
-		 */
-		public static Pair<String, Integer> readString(byte[] bytes) {
-			final StringBuilder builder = new StringBuilder();
-			int count = readByte(bytes[0]);
-			for (int i = 1; i <= count; i++) {
-				builder.appendCodePoint(bytes[i]);
+	@Override
+	public byte[] encode() {
+		final byte[] bytes = new byte[countBytes + 1];
+		bytes[0] = Integer.valueOf(countBytes).byteValue();
+		byte[] valBytes = value.getBytes();
+		System.arraycopy(valBytes, 0, bytes, 1, valBytes.length);
+		return bytes;
+	}
+
+	@Override
+	public int countBytes() {
+		return countBytes;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+}
+
+final class Varuint implements BigType<Long> {
+	private final Long value;
+	private final int countBytes;
+
+	Varuint(byte[] bytes) {
+		long value = 0;
+		int bitSize = 0;
+		int read;
+
+		int index = 0;
+		do {
+			read = bytes[index++];
+			value += ((long) read & 0x7f) << bitSize;
+			bitSize += 7;
+			if (bitSize >= 64) {
+				throw new ArithmeticException("ULEB128 value exceeds maximum value for long type.");
 			}
-			return new Pair<>(builder.toString(), count + 1);
+		} while ((read & 0x80) != 0);
+
+		this.value = value;
+		this.countBytes = index;
+	}
+
+	@Override
+	public Long val() {
+		return value;
+	}
+
+	@Override
+	public byte[] encode() {
+		long val = value;
+		ArrayList<Byte> bytes = new ArrayList<>();
+		do {
+			byte b = (byte) (val & 0x7f);
+			val >>= 7;
+			if (value != 0) {
+				b |= 0x80;
+			}
+			bytes.add(b);
+		} while (val != 0);
+
+		byte[] ret = new byte[bytes.size()];
+		for (int i = 0; i < bytes.size(); i++) {
+			ret[i] = bytes.get(i);
 		}
-	}
-}
-
-record Packet(int length, Payload payload, int crc8) {
-}
-
-record Payload(long src, long dst, long serial, DevType dev_type, Command cmd, CmdBody cmd_body) {
-}
-
-abstract class CmdBody {
-}
-
-class CmdBodyTick extends CmdBody {
-	private final long timestamp;
-
-	CmdBodyTick(long timestamp) {
-		this.timestamp = timestamp;
+		return ret;
 	}
 
-	public long getTimestamp() {
-		return timestamp;
+	@Override
+	public int countBytes() {
+		return countBytes;
 	}
 
 	@Override
 	public String toString() {
-		return "CmdBodyTick{" +
-			"timestamp=" + timestamp +
-			'}';
+		return value.toString();
 	}
 }
 
-class CmdBodyDevName extends CmdBody {
-	private final String dev_name;
+final class TArray<T> implements BigType<T> {
+	private final List<T> list;
+	private final int countBytes;
 
-	CmdBodyDevName(String devName) {
-		dev_name = devName;
+	TArray(byte[] bytes, Function<byte[], Pair<T, Integer>> handler) {
+		int readIndex = 0;
+		int count = new TByte(bytes[readIndex++]).val();
+		final List<T> arrayList = new ArrayList<>(count);
+		for (; count > 0; count--) {
+			Pair<T, Integer> pair = handler.apply(Arrays.copyOfRange(bytes, readIndex, bytes.length));
+			readIndex += pair.right();
+
+			arrayList.add(pair.left());
+		}
+
+		this.list = arrayList;
+		this.countBytes = readIndex;
 	}
 
-	public String getDev_name() {
-		return dev_name;
+	public List<T> list() {
+		return list;
+	}
+
+	@Override
+	public T val() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public byte[] encode() {
+		return null; //TODO
+	}
+
+	@Override
+	public int countBytes() {
+		return countBytes;
 	}
 
 	@Override
 	public String toString() {
-		return "CmdBodyDevName{" +
-			"dev_name='" + dev_name + '\'' +
-			'}';
+		return list.toString();
 	}
 }
 
-class CmdBodyWhereForSensor extends CmdBody {
-	private final String dev_name;
-	private final DevProps dev_props;
-
-	CmdBodyWhereForSensor(String devName, DevProps devProps) {
-		dev_name = devName;
-		dev_props = devProps;
-	}
-
-	public String getDev_name() {
-		return dev_name;
-	}
-
-	public DevProps getDev_props() {
-		return dev_props;
-	}
-
-	@Override
-	public String toString() {
-		return "CmdBodyWhereForSensor{" +
-			"dev_name='" + dev_name + '\'' +
-			", dev_props=" + dev_props +
-			'}';
-	}
-}
-
-class CmdBodyWhereForSwitch extends CmdBody {
-	private final String dev_name;
-	private final DevPropsNames dev_ames;
-
-	CmdBodyWhereForSwitch(String devName, DevPropsNames devAmes) {
-		dev_name = devName;
-		dev_ames = devAmes;
-	}
-
-	public String getDev_name() {
-		return dev_name;
-	}
-
-	public DevPropsNames getDev_ames() {
-		return dev_ames;
-	}
-
-	@Override
-	public String toString() {
-		return "CmdBodyWhereForSwitch{" +
-			"dev_name='" + dev_name + '\'' +
-			", dev_ames=" + dev_ames +
-			'}';
-	}
-}
-
-record DevProps(int sensors, List<Trigger> triggers) {
-}
-
-record Trigger(int op, long value, String name) {
-}
-
-record DevPropsNames(List<String> dev_names) {
-}
-
-enum Command {
+enum Cmd {
 	WHOISHERE(0x01),
 	IAMHERE(0x02),
 	GETSTATUS(0x03),
@@ -274,10 +347,10 @@ enum Command {
 	SETSTATUS(0x05),
 	TICK(0x06);
 
-	Command(int b) {
+	Cmd(int b) {
 	}
 
-	public static Command of(int b) {
+	public static Cmd of(int b) {
 		return values()[b - 1];
 	}
 }
@@ -298,74 +371,6 @@ enum DevType {
 	}
 }
 
-record Pair<T, U> (T one, U two) {
-}
 
-//final class Uleb128 {
-//	private final static int BITS_LONG = 64;
-//	private final static int MASK_DATA = 0x7f;
-//	private final static int MASK_CONTINUE = 0x80;
-//	private final long value;
-//
-//	private Uleb128(long value) {
-//		this.value = value;
-//	}
-//
-//	public static Uleb128 fromByteStream(ByteArrayInputStream bytes) throws IOException {
-//		return new Uleb128(decode(bytes));
-//	}
-//
-//	public static Uleb128 fromLong(long value) {
-//		return new Uleb128(value);
-//	}
-//
-//	private static long decode(ByteArrayInputStream bytes) throws IOException {
-//		long value = 0;
-//		int bitSize = 0;
-//		int read;
-//
-//		do {
-//			if ((read = bytes.read()) == -1) {
-//				throw new IOException("Unexpected EOF");
-//			}
-//
-//			value += ((long) read & MASK_DATA) << bitSize;
-//			bitSize += 7;
-//			if (bitSize >= BITS_LONG) {
-//				throw new ArithmeticException("ULEB128 value exceeds maximum value for long type.");
-//			}
-//
-//		} while ((read & MASK_CONTINUE) != 0);
-//		return value;
-//	}
-//
-//	private static byte[] encode(long value) {
-//		ArrayList<Byte> bytes = new ArrayList<>();
-//		do {
-//			byte b = (byte) (value & MASK_DATA);
-//			value >>= 7;
-//			if (value != 0) {
-//				b |= MASK_CONTINUE;
-//			}
-//			bytes.add(b);
-//		} while (value != 0);
-//
-//		byte[] ret = new byte[bytes.size()];
-//		for (int i = 0; i < bytes.size(); i++) {
-//			ret[i] = bytes.get(i);
-//		}
-//		return ret;
-//	}
-//
-//	public long asLong() {
-//		return value;
-//	}
-//
-//	public byte[] asBytes() {
-//		return encode(value);
-//	}
-//
-//	public String toString() {
-//		return Long.toString(value);
-//	}
-//}
+record Pair<T, U>(T left, U right) {
+}
